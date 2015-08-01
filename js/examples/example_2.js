a054d497def7aaa0374ac222f5b191b48b5a5f30
F. Javier R. Donado <fjrd84@gmail.com>
2015-08-01 14:28:50 +0200

    Added more examples.

---
+++ js/examples/example_2.js
@@ -0,0 +1,118 @@
+var   w = 400,
+    h = 400;
+
+var circleWidth = 5;
+
+var palette = {
+    "lightgray": "#819090",
+    "gray": "#708284",
+    "mediumgray": "#536870",
+    "darkgray": "#475B62",
+
+    "darkblue": "#0A2933",
+    "darkerblue": "#042029",
+
+    "paleryellow": "#FCF4DC",
+    "paleyellow": "#EAE3CB",
+    "yellow": "#A57706",
+    "orange": "#BD3613",
+    "red": "#D11C24",
+    "pink": "#C61C6F",
+    "purple": "#595AB7",
+    "blue": "#2176C7",
+    "green": "#259286",
+    "yellowgreen": "#738A05"
+}
+
+var nodes = [
+    { name: "Parent"},
+    { name: "child1"},
+    { name: "child2", target: [0]},
+    { name: "child3", target: [0]},
+    { name: "child4", target: [1]},
+    { name: "child5", target: [0, 1, 2, 3]}
+];
+
+var links = [];
+
+for (var i = 0; i< nodes.length; i++) {
+    if (nodes[i].target !== undefined) {
+        for (var x = 0; x< nodes[i].target.length; x++ ) {
+            links.push({
+                source: nodes[i],
+                target: nodes[nodes[i].target[x]]
+            })
+        }
+    }
+}
+
+var myChart = d3.select('#example-2')
+    .append('svg')
+    .attr('width', w)
+    .attr('height', h)
+
+var force = d3.layout.force()
+    .nodes(nodes)
+    .links([])
+    .gravity(0.3)
+    .charge(-1000)
+    .size([w, h])
+
+var link = myChart.selectAll('line')
+    .data(links).enter().append('line')
+    .attr('stroke', palette.gray)
+
+var node = myChart.selectAll('circle')
+    .data(nodes).enter()
+    .append('g')
+    .call(force.drag);
+
+node.append('circle')
+    .attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) { return d.y; })
+    .attr('r', circleWidth )
+    .attr('fill', function(d, i) {
+        if (i>0) { return palette.pink }
+        else { return palette.blue }
+    })
+
+node.append('text')
+    .text(function(d) { return d.name})
+    .attr('font-family', 'Roboto Slab')
+    .attr('fill', function(d, i) {
+        if (i>0) { return palette.mediumgray}
+        else { return palette.yellowgreen}
+    })
+    .attr('x', function(d, i) {
+        if (i>0) { return circleWidth + 4 }
+        else { return circleWidth -15 }
+    })
+    .attr('y', function(d, i) {
+        if (i>0) { return circleWidth }
+        else { return 8 }
+    })
+    .attr('text-anchor', function(d, i) {
+        if (i>0) { return 'beginning' }
+        else { return 'end'}
+    })
+    .attr('font-size',  function(d, i) {
+        if (i>0) { return '1em' }
+        else { return '1.8em'}
+    })
+
+force.on('tick', function(e) {
+    node.attr('transform', function(d, i) {
+        return 'translate('+ d.x +', '+ d.y +')';
+    })
+
+    link
+        .attr('x1', function(d) { return d.source.x })
+        .attr('y1', function(d) { return d.source.y })
+        .attr('x2', function(d) { return d.target.x })
+        .attr('y2', function(d) { return d.target.y })
+})
+
+
+force.start();
+
+
---
+++ js/examples/example_3.js
@@ -0,0 +1,123 @@
+/* This is an example from lynda.com. All rights reserved. */
+
+
+var bardata = [];
+d3.tsv('js/examples/data-example-3.tsv', function(data) {
+    console.log(data);
+
+    for (key in data) {
+        bardata.push(data[key].value)
+    }
+
+    var margin = { top: 30, right: 30, bottom: 40, left:50 }
+
+    var height = 400 - margin.top - margin.bottom,
+        width = 600 - margin.left - margin.right,
+        barWidth = 50,
+        barOffset = 5;
+
+    var tempColor;
+
+    var colors = d3.scale.linear()
+        .domain([0, bardata.length*.33, bardata.length*.66, bardata.length])
+        .range(['#B58929','#C61C6F', '#268BD2', '#85992C'])
+
+    var yScale = d3.scale.linear()
+        .domain([0, d3.max(bardata)])
+        .range([0, height]);
+
+    var xScale = d3.scale.ordinal()
+        .domain(d3.range(0, bardata.length))
+        .rangeBands([0, width], 0.2)
+
+    var tooltip = d3.select('#example-3').append('div')
+        .style('position', 'absolute')
+        .style('padding', '0 10px')
+        .style('background', 'white')
+        .style('opacity', 0)
+
+    var myChart = d3.select('#example-3 .my-chart-3').append('svg')
+        .style('background', '#E7E0CB')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', 'translate('+ margin.left +', '+ margin.top +')')
+        .selectAll('rect').data(bardata)
+        .enter().append('rect')
+        .style('fill', function(d,i) {
+            return colors(i);
+        })
+        .attr('width', xScale.rangeBand())
+        .attr('x', function(d,i) {
+            return xScale(i);
+        })
+        .attr('height', 0)
+        .attr('y', height)
+
+        .on('mouseover', function(d) {
+
+            tooltip.transition()
+                .style('opacity', .9)
+
+            tooltip.html(d)
+                .style('left', (d3.event.pageX - 35) + 'px')
+                .style('top',  (d3.event.pageY - 30) + 'px')
+
+
+            tempColor = this.style.fill;
+            d3.select(this)
+                .style('opacity', .5)
+                .style('fill', 'yellow')
+        })
+
+        .on('mouseout', function(d) {
+            d3.select(this)
+                .style('opacity', 1)
+                .style('fill', tempColor)
+        })
+
+    myChart.transition()
+        .attr('height', function(d) {
+            return yScale(d);
+        })
+        .attr('y', function(d) {
+            return height - yScale(d);
+        })
+        .delay(function(d, i) {
+            return i * 20;
+        })
+        .duration(1000)
+        .ease('elastic')
+
+    var vGuideScale = d3.scale.linear()
+        .domain([0, d3.max(bardata)])
+        .range([height, 0])
+
+    var vAxis = d3.svg.axis()
+        .scale(vGuideScale)
+        .orient('left')
+        .ticks(10)
+
+    var vGuide = d3.select('#example-3 svg').append('g')
+    vAxis(vGuide)
+    vGuide.attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+    vGuide.selectAll('path')
+        .style({ fill: 'none', stroke: "#000"})
+    vGuide.selectAll('line')
+        .style({ stroke: "#000"})
+
+    var hAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient('bottom')
+        .tickValues(xScale.domain().filter(function(d, i) {
+            return !(i % (bardata.length/5));
+        }))
+
+    var hGuide = d3.select('#example-3 svg').append('g')
+    hAxis(hGuide)
+    hGuide.attr('transform', 'translate(' + margin.left + ', ' + (height + margin.top) + ')')
+    hGuide.selectAll('path')
+        .style({ fill: 'none', stroke: "#000"})
+    hGuide.selectAll('line')
+        .style({ stroke: "#000"})
+});
