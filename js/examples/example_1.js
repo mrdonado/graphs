68c062946ca810c78f03074c8adbe5759f287238
F. Javier R. Donado <fjrd84@gmail.com>
2015-08-03 21:48:23 +0200

    Graphics made responsive.

--- js/examples/example_1.js
+++ js/examples/example_1.js
@@ -5,21 +5,34 @@ var width = 500;
 
 var height = 500;
 
-var svg =
-    d3.select('#example-1')
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
-
 // Example of a color scale
 var color = d3.scale.linear()
     .domain([0, d3.max(dataArray)])
     .range(['red', 'blue']);
 
+// Example of a linear width scale
 var widthScale =
     d3.scale.linear()
         .domain([0, d3.max(dataArray)])
         .range([0, width]);
+
+var axisex1 =
+    d3.svg.axis()
+        .ticks(5)
+        .scale(widthScale);
+
+
+var svg =
+        d3.select('#example-1')
+            .append('svg')
+//            .attr('width', width)
+//            .attr('height', height)
+            .append('g')
+            .attr('transform', 'translate(20, 0)')
+            .attr('viewBox', '0 0 ' + width + ' ' + height)
+            .attr('preserveAspectRatio', 'xMidYMid')
+            .attr('data-aspect', width/height)
+    ;
 
 var bars =
     svg.selectAll('rect')
@@ -36,3 +49,7 @@ var bars =
         .attr('y', function (d, i) {
             return i * 100
         });
+
+svg.append('g')
+    .attr('transform', 'translate(0,400)')
+    .call(axisex1);
--- js/examples/example_2.js
+++ js/examples/example_2.js
@@ -1,4 +1,4 @@
-var   w = 400,
+var w = 400,
     h = 400;
 
 var circleWidth = 5;
@@ -25,19 +25,19 @@ var palette = {
 }
 
 var nodes = [
-    { name: "Parent"},
-    { name: "child1"},
-    { name: "child2", target: [0]},
-    { name: "child3", target: [0]},
-    { name: "child4", target: [1]},
-    { name: "child5", target: [0, 1, 2, 3]}
+    {name: "Parent"},
+    {name: "child1"},
+    {name: "child2", target: [0]},
+    {name: "child3", target: [0]},
+    {name: "child4", target: [1]},
+    {name: "child5", target: [0, 1, 2, 3]}
 ];
 
 var links = [];
 
-for (var i = 0; i< nodes.length; i++) {
+for (var i = 0; i < nodes.length; i++) {
     if (nodes[i].target !== undefined) {
-        for (var x = 0; x< nodes[i].target.length; x++ ) {
+        for (var x = 0; x < nodes[i].target.length; x++) {
             links.push({
                 source: nodes[i],
                 target: nodes[nodes[i].target[x]]
@@ -46,10 +46,16 @@ for (var i = 0; i< nodes.length; i++) {
     }
 }
 
+
+
 var myChart = d3.select('#example-2')
     .append('svg')
     .attr('width', w)
     .attr('height', h)
+    .attr('viewBox', '0 0 ' + w + ' ' + h)
+    .attr('preserveAspectRatio', 'xMidYMid')
+    .attr('data-aspect', w/h)
+
 
 var force = d3.layout.force()
     .nodes(nodes)
@@ -68,51 +74,88 @@ var node = myChart.selectAll('circle')
     .call(force.drag);
 
 node.append('circle')
-    .attr('cx', function(d) { return d.x; })
-    .attr('cy', function(d) { return d.y; })
-    .attr('r', circleWidth )
-    .attr('fill', function(d, i) {
-        if (i>0) { return palette.pink }
-        else { return palette.blue }
+    .attr('cx', function (d) {
+        return d.x;
+    })
+    .attr('cy', function (d) {
+        return d.y;
+    })
+    .attr('r', circleWidth)
+    .attr('fill', function (d, i) {
+        if (i > 0) {
+            return palette.pink
+        }
+        else {
+            return palette.blue
+        }
     })
 
 node.append('text')
-    .text(function(d) { return d.name})
+    .text(function (d) {
+        return d.name
+    })
     .attr('font-family', 'Roboto Slab')
-    .attr('fill', function(d, i) {
-        if (i>0) { return palette.mediumgray}
-        else { return palette.yellowgreen}
+    .attr('fill', function (d, i) {
+        if (i > 0) {
+            return palette.mediumgray
+        }
+        else {
+            return palette.yellowgreen
+        }
     })
-    .attr('x', function(d, i) {
-        if (i>0) { return circleWidth + 4 }
-        else { return circleWidth -15 }
+    .attr('x', function (d, i) {
+        if (i > 0) {
+            return circleWidth + 4
+        }
+        else {
+            return circleWidth - 15
+        }
     })
-    .attr('y', function(d, i) {
-        if (i>0) { return circleWidth }
-        else { return 8 }
+    .attr('y', function (d, i) {
+        if (i > 0) {
+            return circleWidth
+        }
+        else {
+            return 8
+        }
     })
-    .attr('text-anchor', function(d, i) {
-        if (i>0) { return 'beginning' }
-        else { return 'end'}
+    .attr('text-anchor', function (d, i) {
+        if (i > 0) {
+            return 'beginning'
+        }
+        else {
+            return 'end'
+        }
     })
-    .attr('font-size',  function(d, i) {
-        if (i>0) { return '1em' }
-        else { return '1.8em'}
+    .attr('font-size', function (d, i) {
+        if (i > 0) {
+            return '1em'
+        }
+        else {
+            return '1.8em'
+        }
     })
 
-force.on('tick', function(e) {
-    node.attr('transform', function(d, i) {
-        return 'translate('+ d.x +', '+ d.y +')';
+force.on('tick', function (e) {
+    node.attr('transform', function (d, i) {
+        return 'translate(' + d.x + ', ' + d.y + ')';
     })
 
     link
-        .attr('x1', function(d) { return d.source.x })
-        .attr('y1', function(d) { return d.source.y })
-        .attr('x2', function(d) { return d.target.x })
-        .attr('y2', function(d) { return d.target.y })
+        .attr('x1', function (d) {
+            return d.source.x
+        })
+        .attr('y1', function (d) {
+            return d.source.y
+        })
+        .attr('x2', function (d) {
+            return d.target.x
+        })
+        .attr('y2', function (d) {
+            return d.target.y
+        })
 })
 
 
 force.start();
-
 
