18e3c68b6c9c4981839158545c1f765ee9917b19
F. Javier R. Donado <fjrd84@gmail.com>
2015-08-05 21:36:04 +0200

    Linear graph modified to display multiple sources.

--- js/examples/example_4.js
+++ js/examples/example_4.js
@@ -4,6 +4,27 @@ var linearGraph = function () {
         width = 500 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
+    // All the data sources. Each data source is a line in the graph.
+    var dataSources = ['js/data/data1.tsv', 'js/data/data2.tsv', 'js/data/data3.tsv', 'js/data/data4.tsv'];
+
+    var numSources = dataSources.length;
+
+    // It stores the received (and parsed) data
+    var allData = [];
+
+    // Global maximum and minimum values, for all the data sources 
+    // (calculated when the information is received).
+    var maxX;
+    var maxY;
+    var minX;
+    var minY;
+
+    // A different color for each data source
+    var lineColors =
+        d3.scale.linear()
+            .domain([0, numSources])
+            .range(['red', 'blue']);
+
     var parseDate = d3.time.format("%d-%b-%y").parse;
 
     var x = d3.time.scale()
@@ -27,9 +48,10 @@ var linearGraph = function () {
             return x(d.date);
         })
         .y(function (d) {
-            return y(d.close);
+            return y(d.value);
         });
 
+    // A responsive SVG is generated. It will be used as a canvas for the graphic.
     var svg = d3.select("#example-4").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -39,20 +61,62 @@ var linearGraph = function () {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    d3.tsv("data.tsv", function (error, data) {
-        if (error) throw error;
+    // The sources are retrieved.
+    for (var i = 0; i < numSources; i += 1) {
+        d3.tsv(dataSources[i], function (error, data) {
+            if (error) throw error;
+            data.forEach(function (d) {
+                d.date = parseDate(d.date);
+                d.value = +d.value;
+            });
+            allData.push(data);
+            updateDomain(data);
+            paintAll(); // paintAll will paint all the sources when the info is ready.
 
-        data.forEach(function (d) {
-            d.date = parseDate(d.date);
-            d.close = +d.close;
         });
+    }
 
-        x.domain(d3.extent(data, function (d) {
-            return d.date;
-        }));
-        y.domain(d3.extent(data, function (d) {
-            return d.close;
-        }));
+    // It updates the domain for all the sources
+    var updateDomain = function (data) {
+        'use strict';
+        var currentMaxX,
+            currentMaxY,
+            currentMinX,
+            currentMinY;
+        currentMaxX = d3.max(data, function (d) {
+            return d.date
+        });
+        currentMinX = d3.min(data, function (d) {
+            return d.date
+        });
+        currentMaxY = d3.max(data, function (d) {
+            return d.value
+        });
+        currentMinY = d3.min(data, function (d) {
+            return d.value
+        });
+        maxX = d3.max([currentMaxX, maxX]);
+        maxY = d3.max([currentMaxY, maxY]);
+        minX = d3.min([currentMinX, minX]);
+        minY = d3.min([currentMinY, minY]);
+    }
+
+    var paintAll = function () {
+        // Paint nothing if the information is not there yet.
+        if (allData.length < numSources) {
+            return;
+        }
+        paintAxis();
+        for (var i = 0; i < numSources; i += 1) {
+            paintDataSource(allData[i], lineColors(i));
+        }
+    };
+
+    // It defines the domain and paints the axes.
+    var paintAxis = function () {
+
+        x.domain([minX, maxX]);
+        y.domain([minY, maxY]);
 
         svg.append("g")
             .attr("class", "x axis")
@@ -66,12 +130,16 @@ var linearGraph = function () {
             .attr("transform", "rotate(-90)")
             .attr("y", 6)
             .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text("Price ($)");
+            .style("text-anchor", "end");
+    };
+
+
+    var paintDataSource = function (data, color) {
 
         var path = svg.append("path")
             .datum(data)
             .attr("class", "line")
+            .attr("stroke", color)
             .attr("d", line);
 
         var totalLength = path.node().getTotalLength();
@@ -82,9 +150,9 @@ var linearGraph = function () {
             .transition()
             .duration(2500)
             .ease("linear")
-            .attr("stroke-dashoffset", 2*totalLength);
-
-    });
+            .attr("stroke-dashoffset", 0);
+    };
 };
 
+// The function to generate the whole thing is called here
 linearGraph();
